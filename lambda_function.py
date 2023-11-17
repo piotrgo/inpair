e2cf09520e293b90376e4016238c2ca304bdd806
Piotr Gospodarek <pgospodarek@outlook.com>
2023-11-17 15:34:07 +0100

    Initial commit

---
+++ lambda_function.py
@@ -0,0 +1,97 @@
+import requests
+import folium
+import boto3
+import os
+
+def lambda_handler(event, context):
+    # Read API token from environment variable
+    env_variable_name = 'INPOST_API_TOKEN'
+    token = os.environ.get(env_variable_name)
+
+    if not token:
+        return {
+            'statusCode': 500,
+            'body': f'Error: {env_variable_name} environment variable not set.'
+        }
+
+    # API location and auth
+    url = "https://api.inpost.pl/v1/points"
+    headers = {"Authorization": f"Bearer {token}"}
+    
+    # Create a folium map centered on Poland
+    map_center = [52, 19]
+    my_map = folium.Map(location=map_center, zoom_start=7)
+    
+    # Color coding for different air quality indexes from points
+    air_quality_index_to_color_code = {
+        "VERY_GOOD": "green",
+        "GOOD": "lightgreen",
+        "SATISFACTORY": "orange",
+        "MODERATE": "orange",
+        "BAD": "red",
+        "VERY_BAD": "darkred"
+    }
+
+    # Get number of pages with points
+    page_count = requests.get(url, headers=headers)
+    if page_count.status_code == 200:
+        total_pages=page_count.json()["total_pages"]
+    else:
+        print(page_count.status_code)
+    
+    # Go through all the pages to get all the points that report air_index_level
+    # add them to the map with appropriate colour
+
+    for i in range(1,total_pages+1):
+        params = {'page': i,}
+        print(i)
+        response = requests.get(url, headers=headers, params=params)
+        if response.status_code == 200:
+            #points = response.json()
+            points=response.json()["items"]
+            
+            for point in points:
+                if point["air_index_level"] is not None:
+                    color_code = air_quality_index_to_color_code[point["air_index_level"]]
+                    air_quality_index = point["air_index_level"]
+                    folium.CircleMarker(location=[point['location']['latitude'], 
+                                            point['location']['longitude']],
+                                            radius=25,
+                                            fill_opacity=0.6,
+                                            fill_color=color_code,
+                                            stroke=False,
+                                            tooltip=air_quality_index
+                                            ).add_to(my_map)
+    
+        else:
+            print(response.status_code)
+    # Save the map to an HTML file
+    my_map.save("/tmp/index.html")
+    
+    # Upload HTML file to S3
+    bucket_name = "inpair"
+    s3_object_key = "index.html"
+    content_type = "text/html"
+
+    with open("/tmp/index.html", 'rb') as file:
+        file_content = file.read()
+
+    try:
+        s3 = boto3.client('s3')
+        s3.put_object(Body=file_content, Bucket=bucket_name, Key=s3_object_key, ContentType=content_type)
+        print(f"HTML file uploaded to S3: s3://{bucket_name}/{s3_object_key}")
+    except FileNotFoundError:
+        return {
+            'statusCode': 500,
+            'body': f'The file {s3_object_key} was not found.'
+        }
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': f'An error occurred: {e}'
+        }
+
+    return {
+        'statusCode': 200,
+        'body': 'HTML file uploaded to S3 successfully.'
+    }
